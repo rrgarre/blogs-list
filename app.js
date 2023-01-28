3311b558d101ef4c272a74359ad53dd419566db8
garredev <rrgarre@gmail.com>
2023-01-28 14:44:45 +0100

    controllers separados

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
 const config = require('./utils/config')
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blog')
+
 
 mongoose.connect(config.MONGODB_URI)
+
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app
